b434fc319dc7da7b982dc61bcf893cc4f8b30c12
javiesp <espinajaviera1312@gmail.com>
2024-04-03 08:52:35 -0300

    Frontend intial commit

---
+++ components/Layout/logo/Logo.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+<template>
+    <div class="logo">
+      <NuxtLink to="/" class="d-flex">
+        <img src="/images/logos/citt_logo.png" />
+      </NuxtLink>
+    </div>
+  </template>
